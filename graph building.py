c70c0d2b993fbd609f5063fc6904e1b414e1d334
maxatlas <66986441+maxatlas@users.noreply.github.com>
2020-07-03 14:21:16 +1000

    Add files via upload

--- graph building.py
+++ graph building.py
@@ -22,18 +22,28 @@ def get_e_sig_mean(docs):
 
 	return out/n
 
-def get_edge_weight_per_doc(e1, e2, tf):
-	e1_tf, e2_tf = tf.get(e1), tf.get(e2)
-	if e1_tf and e2_tf: return e1_tf+e2_tf
-	else: return 0
+def get_edge_weights_per_doc(tf):
+	'''
+		tf: output of get_e_sig
+		key: string of 2 keys
+	'''
+	out = pd.Series([], dtype="float64")
+	for i, key0 in enumerate(tf.index):
+		for key1 in tf.index[i+1:]:
+			key, value = json.dumps(list(set((key0, key1)))), tf[key0]+tf[key1]
+			out = out.add(pd.Series({key:value}), fill_value=0)
+	return out
 
-def get_edge_weight_all_docs(e1, e2, docs):
+def get_edge_weights_all_docs(docs):
 	'''
 		docs: [doc]
 		doc: [NER entities].
+
 	'''
-	out = 0
-	for doc in docs: out+=get_edge_weight_per_doc(e1, e2, get_tf_table(doc))
+	out = pd.Series([], dtype="float64")
+	for doc in docs: 
+		tf = get_e_sig(doc)
+		out = out.add(get_edge_weights_per_doc(tf), fill_value=0)
 	return out
 
 def get_knowledge_graph(docs):
@@ -48,17 +58,21 @@ def get_knowledge_graph(docs):
 
 
 	return {
-		"entity size":1,
-		"edge weight":2
+		"e_sig_mean": get_e_sig_mean(docs),
+		"edge_weights": get_edge_weights_all_docs(docs)
 	}
 
 
 if __name__ == '__main__':
 	from preprocessing import texts2NER
-	sample_texts = json.load(open("4.Get Tweets/2020-03-28.json"))[:10]
+	sample_texts = json.load(open("4.Get Tweets/2020-03-28.json"))[:1000]
 	l = texts2NER(sample_texts)
+	out = get_edge_weights_all_docs(l)
 	# out = get_e_sig_mean(l)
-	# pprint(out)
-	for NERs in l:
-		pprint(get_e_sig(NERs))
+	pprint(out)
+	# for NERs in l:
+	# 	tf=get_e_sig(NERs)
+	# 	ew = get_edge_weights_per_doc(tf)
+
+	# 	pprint(ew)
 	
