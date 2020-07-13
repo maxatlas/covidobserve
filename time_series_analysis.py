d24f4c868d34f09b9ab83a3eea15d08bd1afbe06
maxatlas <66986441+maxatlas@users.noreply.github.com>
2020-07-13 13:03:04 +1000

    Add files via upload

--- time_series_analysis.py
+++ time_series_analysis.py
@@ -17,24 +17,39 @@ from preprocessing import texts2NER, NER2texts
 from graph_building import get_knowledge_graph
 
 
-def divide2blocks(e_sigs):
+def get_e_sigs(e_sigs, indexes):
 	'''
 		e_sigs: {e_text:significance #(count/sum)}
 		
 		Space and time optimization needed?
 	'''
-	
+	assert len(e_sigs)==len(indexes)
+
 	out = pd.DataFrame(data=[]).rename_axis("NERs")
-	for e_sig in e_sigs: 
+	for name, e_sig in zip(indexes, e_sigs): 
 		e_sig = pd.Series(e_sig).to_frame().rename_axis("NERs")
+		e_sig.columns=[name]
 		out = out.merge(e_sig, left_on="NERs", right_on="NERs", how="outer")
 	
 	return out
 
+def get_peaking_entities(e_sigs, e_sigs_rolling_mean, e_sigs_rolling_std):
+	return e_sigs-e_sigs_rolling_mean>e_sigs_rolling_std
+
 if __name__ == '__main__':
 	folder = "5.Graphs"
-	graphs = [json.load(open(p.join(folder, file)))["e_sigs_mean"] for file in listdir(folder)]
-	graphs= graphs[:5]
+	files = sorted(listdir(folder))
+	graphs = [json.load(open(p.join(folder, file)))["e_sigs_mean"] for file in files]
+	indexes = [index[:-5] for index in files]
+	# graphs= graphs[:5]
 
-	out = divide2blocks(graphs)
-	pprint(out)
+	e_sigs = get_e_sigs(graphs, indexes)
+	# out.to_csv("e_sigs_mean.csv")
+	e_sigs_rolling_mean = e_sigs.rolling(3).mean()
+	# out.to_csv("e_sigs_rolling_mean.csv")
+	e_sigs_rolling_std = e_sigs.rolling(3).std()
+	e_sigs_rolling_std.to_csv("e_sigs_rolling_std.csv")
+	
+	out = e_sigs - e_sigs_rolling_mean > e_sigs_rolling_std
+	out.to_csv("peaking_entities.csv")
+	# pprint(out)
