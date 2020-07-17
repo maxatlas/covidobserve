055f06e12bf8e5f230c37d39b00490d34564802e
maxatlas <66986441+maxatlas@users.noreply.github.com>
2020-07-17 19:09:15 +1000

    Delete runme.py

--- runme.py
+++
@@ -1,8 +0,0 @@
-import subprocess, shlex
-
-def run_script(command):
-	args = shlex.split(command)
-	return subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
-
-if __name__ == '__main__':
-	print("YES!", "YEAH!")
